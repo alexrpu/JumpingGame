3f73b48ab19417bf84df4bda08ef955af00ffac7
Alex Pu <alexander.r.pu@gmail.com>
2023-02-27 18:34:07 -0800

    changed camera size

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -13,7 +13,7 @@ public class GameManager : MonoBehaviour
 	for (int i = 0; i < platformCount; i++)
 	{
 	    spawnPosition.y += Random.Range(.5f,2f);
-	    spawnPosition.x = Random.Range(-5f,5f);
+	    spawnPosition.x = Random.Range(-3f,3f);
 	    Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
 	}
     }
@@ -21,6 +21,5 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
